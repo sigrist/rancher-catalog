06d3b8f6727b712cac371f28bd798d3e48998213
Paulo Sigrist <paulo.sigrist@domrock.com.br>
2018-02-27 10:43:43 -0300

    v4 -

--- index.js
+++ index.js
@@ -2,14 +2,37 @@
 var express = require('express');
 var app = express();
 var os = require('os')
-var version = "v3"
+var version = "v4"
+const http = require('http');
+ 
 
 app.get('/', function (req, res) {
   
   var msg = os.hostname()  
   console.log(msg);
 
-  res.send(msg + " / " + version);
+  var url = req.param("url", "localhost:3013")
+
+  console.log(url)
+  http.get(url, (resp) => {
+    let data = '';
+  
+    // A chunk of data has been recieved.
+    resp.on('data', (chunk) => {
+      data += chunk;
+    });
+  
+    // The whole response has been received. Print out the result.
+    resp.on('end', () => {
+      res.send(data);
+    });
+ 
+  }).on("error", (err) => {
+    console.log("Error: " + err.message);
+    res.send(msg + " / " + version +" / ERRO: " +err.message);
+  });
+
+  
 });
 
 app.listen(3000, function () {
