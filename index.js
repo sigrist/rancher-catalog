a864249f9df74ed55d32a02fdb28e91d679786c4
Paulo Sigrist <paulo.sigrist@domrock.com.br>
2018-02-27 09:19:11 -0300

    version 2

--- index.js
+++ index.js
@@ -2,13 +2,14 @@
 var express = require('express');
 var app = express();
 var os = require('os')
+var version = "v2"
 
 app.get('/', function (req, res) {
   
   var msg = os.hostname()  
   console.log(msg);
 
-  res.send(msg);
+  res.send(msg + " / " + version);
 });
 
 app.listen(3000, function () {
